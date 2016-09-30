e04f05eed2274a54386d38c41cc2ed19be8a7492
Matteo Cappelletti <matcap94@gmail.com>
2016-09-30 17:33:12 +0200

    Lettura da seriale con interrupt funzionante

--- pic16-serial.X/main.asm
+++ pic16-serial.X/main.asm
@@ -1,48 +1,69 @@
 #include <p16f887.inc>
     LIST p=16f887
-; *** Configurazione microcontrollore ***
+    
+; Configurazione microcontrollore
     __CONFIG _CONFIG1, _INTRC_OSC_NOCLKOUT & _WDT_OFF
 
-RES_VECT    CODE    0x0000            ; processor reset vector
-    GOTO START                   ; go to beginning of program
+RES_VECT CODE 0x0000		; processor reset vector
+    GOTO START			; go to beginning of program
   
-INT_VECT    CODE    0x0004
+INT_VECT CODE 0x0004
     GOTO IRQ
     
-; *** Definizione variabili in memoria condivisa ***
+; Definizione variabili in memoria condivisa
     UDATA_SHR
 
-MAIN_PROG CODE                      ; let linker place main program
+MAIN_PROG CODE			; let linker place main program
 
 IRQ
- retfie
+    BANKSEL RCREG
+    movf RCREG, 0		; Read EUSART rx buffer
+    andlw 0x0F			; Map ASCII to 0-9
+    BANKSEL PORTD
+    movwf PORTD
+    retfie
 
 START
-; setup EUSART 
+    ; Test LEDS
+    BANKSEL PORTD
+    movlw 0x01
+    movwf PORTD
+    BANKSEL TRISD
+    movlw 0x00
+    movwf TRISD
+    
+    ; Setup EUSART
+    BANKSEL TRISC
+    bcf TRISC, RC6		;Set TX pin come output
+    bsf TRISC, RC7		;Set RX pin come input
+    
     BANKSEL TXSTA
     bcf TXSTA,SYNC		;utilizza l'EUSART in modalità asincrona
-    bsf TXSTA,BRGH		;BRGH 1 per avere un baudrate a 9600
+    bsf TXSTA,BRGH		;high-baudrate
     BANKSEL BAUDCTL
     bcf	BAUDCTL,BRG16		;BRG16 0 per avere un baudrate a 9600
     movlw D'25'			;scrive 25 in decimale nel registro W
     BANKSEL SPBRG
     movwf SPBRG			;sposta il valore di W(25) nel registro SPBRG per impostare il baudrate a 9600
-    BANKSEL RCSTA
-    bsf RCSTA,CREN		;abilita il circuito di ricezione
-    bsf RCSTA,SPEN		;abilita la periferica
 
-; abilita interrupt alla ricezione di dati
+    ; Abilita interrupt alla ricezione di dati
     BANKSEL PIE1
     bsf PIE1,RCIE		;abilita l'interrupt in ricezione
     BANKSEL INTCON
     bsf INTCON,PEIE		;abilita l'interrupt della periferica
     bsf INTCON,GIE		;abilita gli interrupt generali
+
+    ; Avvia periferica EUSART
+    BANKSEL RCSTA
+    bsf RCSTA,CREN		;abilita il circuito di ricezione
+    bsf RCSTA,SPEN		;abilita la periferica
     
-sleep_pnt   SLEEP		;punto di sleep
+main_loop
+    ;sleep			;punto di sleep
+    BANKSEL PIR1
+    btfss PIR1, RCIF		;controlla se c'è qualcosa da leggere in seriale
+    goto main_loop		;ritorna al main_loop se il buffer è vuoto
+
     
-    BTFSS PIR1,RCIF		;controlla se c'è qualcosa da leggere in seriale
-	goto sleep_pnt
-    ;INSERIRE QUI IN MEZZO LE OPERAZIONI DA FARE QUANDO SI RISVEGLIA DALLO SLEEP
-   
-    goto sleep_pnt
+    goto main_loop	    
     END
